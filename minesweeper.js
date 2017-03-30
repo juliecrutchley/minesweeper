7f3f3fb95f629f1feb1a5006700e25c86460d424
Daniel Crutchley <dan-julz@hotmail.com>
2017-03-30 19:00:58 +1300

    6.2 complete, count working

--- minesweeper.js
+++ minesweeper.js
@@ -8,61 +8,74 @@ var board = {
       row: 1,
       col: 1,
       isMine: true,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
+
     },
     b ={
       row: 1,
       col: 2,
       isMine: false,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
     c ={
       row: 1,
       col: 3,
       isMine: false,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
     d ={
       row: 2,
       col: 1,
       isMine: true,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
     e ={
       row: 2,
       col: 2,
       isMine: false,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
     f ={
       row: 2,
       col: 3,
       isMine: false,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
     g ={
       row: 3,
       col: 1,
       isMine: false,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
     h ={
       row: 3,
       col: 2,
       isMine: false,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
     i ={
       row: 3,
       col: 3,
       isMine: false,
-      hidden: true
+      hidden: true,
+      surroundingMines: 0
     },
   ],
 }
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
+  for (var i=0; i<board.cells.length; i++) {
+    board.cells[i].surroundingMines =  countSurroundingMines(board.cells[i]);
+  }
   lib.initBoard()
 }
 
@@ -85,5 +98,17 @@ function checkForWin () {
 //
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
+
+
+
+
 function countSurroundingMines (cell) {
+  var count = 0;
+  var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
+  for (var i=0; i<surroundingCells.length; i++) {
+    if (surroundingCells[i].isMine) {
+      count++;
+    }
+  }
+  return count;
 }
